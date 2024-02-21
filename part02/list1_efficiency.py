892dbefe1e3f3becdcb5c5a79773921ee1a8f7a4
Victor <aue228lublupacanov@gmail.com>
2024-02-21 17:02:14 +0200

    save list1_efficiency

--- part02/list1_efficiency.py
+++ part02/list1_efficiency.py
@@ -1,8 +1,39 @@
+import random
+import time
+
 numbers = []
 
 def addNum(a):
     numbers.append(a)
     return numbers
 
-if __name__ == "__main__": 
-    print(addNum(2))
+def removeNum():
+    for _ in range(n):
+        if numbers:
+            numbers.pop()
+    return numbers
+
+n = 10**5
+print('n: ', n)
+
+random.seed(1337)
+a = [random.randint(1, n) for _ in range(n)]
+
+start_time = time.time()
+result = addNum(a)
+end_time = time.time()
+
+print("result: ", numbers)
+print(start_time, end_time)
+print("Adding numbers time: ", round(end_time - start_time, 7), "s")
+
+
+start_time = time.time()
+result = removeNum()
+end_time = time.time()
+
+print("result: ", result)
+print("Removing numbers time: ", round(end_time - start_time, 5), "s")
+
+# if __name__ == "__main__": 
+#     print(addNum(2))
